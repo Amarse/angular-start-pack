79fd7f5848483aafe1941ecb271e018f880c0619
vinnie0416 <vinnie0416@warever.io>
2020-12-30 15:48:32 +0900

    spec.ts test파일 에러없이 수정

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-quick-start'`, () => {
+  it(`should have as title 'warever-webpage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-quick-start');
+    expect(app.title).toEqual('warever-webpage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-quick-start app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('warever-webpage app is running!');
   });
 });
