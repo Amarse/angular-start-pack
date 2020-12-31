9718c889db1ac0237edef3a79d64e8ab862217a4
vinnie0416 <vinnie0416@warever.io>
2020-12-31 18:11:02 +0900

    router 테스트진행중

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {  RouterModule } from '@angular/router';
+import {  Route,RouterModule } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatMenuModule} from '@angular/material/menu';
+import { ContentComponent } from './content/content.component';
+
+
+
+const routes:Route[]=[
+  {path:'', component:HomeComponent},
+  {path:'content', component:ContentComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule,
+    RouterModule.forRoot(routes),
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
