286e6685362af4283a003d0e3b1779e29e54a065
vinnie0416 <vinnie0416@warever.io>
2021-01-05 15:03:21 +0900

    게시판테이블 메인에 생성

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,21 +3,21 @@ import { Component, OnInit } from '@angular/core';
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
-  symbol: string;
+  title:string;
+  
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Hydrogen',title:"board1"},
+  {position: 2, name: 'Helium',title:"board1"},
+  {position: 3, name: 'Lithium',title:"board1"},
+  {position: 4, name: 'Beryllium',title:"board1"},
+  {position: 5, name: 'Boron',title:"board1"},
+  {position: 6, name: 'Carbon',title:"board1"},
+  {position: 7, name: 'Nitrogen',title:"board1"},
+  {position: 8, name: 'Oxygen',title:"board1"},
+  {position: 9, name: 'Fluorine',title:"board1"},
+  {position: 10, name: 'Neon',title:"board1"},
 ];
 
 @Component({
@@ -26,7 +26,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'title', 'name'];
   dataSource = ELEMENT_DATA;
   constructor() {
   
